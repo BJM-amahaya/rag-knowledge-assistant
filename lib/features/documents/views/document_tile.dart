9735f24a88844183103d96a36a03bc282861feea
Takayoshi Amahaya <t.amahaya@benjamin.co.jp>
2026-01-03 11:08:37 +0900

    詳細画面実装

---
+++ lib/features/documents/views/document_tile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:rag_knowledge_assistant_frontend/features/documents/models/document.dart';
+
+class DocumentTile extends StatelessWidget {
+  final Document document;
+  const DocumentTile({super.key, required this.document});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
