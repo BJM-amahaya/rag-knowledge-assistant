d8d6af739d0d2dbb13b10211992e243fb8f5b069
Takayoshi Amahaya <t.amahaya@benjamin.co.jp>
2026-01-05 09:13:35 +0900

    upload_dialog画面実装

---
+++ lib/features/documents/views/upload_dialog.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rag_knowledge_assistant_frontend/features/documents/models/document.dart';
+import 'package:rag_knowledge_assistant_frontend/features/documents/providers/document_provider.dart';
+
+class UploadDialog extends ConsumerStatefulWidget {
+  const UploadDialog({super.key});
+
+  @override
+  ConsumerState<UploadDialog> createState() => _UploadDialogState();
+}
+
+class _UploadDialogState extends ConsumerState<UploadDialog> {
+  final _nameController = TextEditingController();
+  bool _isUploading = false;
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('ドキュメントをアップロード'),
+      content: Column(mainAxisSize: MainAxisSize.min, children: [
+          
+        ],
+        ),
+    );
+  }
+}
